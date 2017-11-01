354d62d68881539bead14644ecc5a511508fc420
AlynaS <abs14e@my.fsu.edu>
2017-10-31 20:48:00 -0400

    Main Menu Creation
    Created a new scene called MainMenu, and used main menu assets to create the main menu. Created a test scene to test the menu functionality. Added colliders to the text pictures to allow players to click buttons. Added script to allow button clicks to change scene. Only Start and Exit buttons coded to react to mouse clicks.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Defining variables for different menu buttons
+	public bool isStart;		// Variable for Start button
+	public bool isSettings;		// Variable for Settings button
+	public bool isExit;			// Variable for Exit button
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseUp () {
+		if (isStart) {
+			SceneManager.LoadScene(1);
+			//Application.LoadLevel (1);
+
+		} else if (isSettings) {
+
+		} else if (isExit) {
+			Application.Quit();
+		}
+	}
+}
