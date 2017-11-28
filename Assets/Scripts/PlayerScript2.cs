b1d16f8490ed60db097572ccf9ac5747b5da7091
AlynaS <abs14e@my.fsu.edu>
2017-11-28 03:09:29 -0500

    New Scene - Script Update
    I updated the player script to pick up gun and switch to gun animations, needs some adjustments.
    Also created a new scene (Dinosaur scene) and added new sprites to use as background.
    Changed the settings so the menu "start" button goes to dinosaur level

--- Assets/Scripts/PlayerScript2.cs
+++ Assets/Scripts/PlayerScript2.cs
@@ -15,6 +15,7 @@ public class PlayerScript2 : MonoBehaviour {
 	private float y_pos;
 	private float z_pos;
 	private int health = 8;
+	private bool Gun = false;
 
 	// Use this for initialization
 	void Start () {
@@ -35,43 +36,79 @@ public class PlayerScript2 : MonoBehaviour {
 		transform.position += new Vector3 (moveHorizontal, 0f, 0f) * Time.deltaTime * speed;
 
 
-		if(Input.GetButton("Jump") && isGrounded){	//Jumping
-			my_animator.SetBool ("isJumpingB", true);
-			my_animator.SetBool ("isWalkingB", false);
-			my_animator.SetBool ("isIdleB", false);
+		if (Gun) {
 
-			//rb.AddForce (new Vector2 (0f, 500f));
-			rb.velocity = new Vector2 (0f, jumpheight);
-			isGrounded = false;
+			if (Input.GetButton ("Jump") && isGrounded) {	//Jumping
+				my_animator.SetBool ("GunJump", true);
+				my_animator.SetBool ("GunRun", false);
+				my_animator.SetBool ("GunIdle", false);
+
+				//rb.AddForce (new Vector2 (0f, 500f));
+				rb.velocity = new Vector2 (0f, jumpheight);
+				isGrounded = false;
+			}
+
+			else if (moveHorizontal > 0) {		//Moving right (while jumping or running)
+				transform.localScale = new Vector3 (x_pos, y_pos, z_pos);
+
+				if (isGrounded) {				//Moving right while running
+					my_animator.SetBool ("GunRun", true);
+					my_animator.SetBool ("GunIdle", false);
+				}
+			} else if (moveHorizontal < 0) {		//Moving left (while jumping or running)
+				transform.localScale = new Vector3 (-x_pos, y_pos, z_pos);
+
+				if (isGrounded) {				//Moving left while running
+					my_animator.SetBool ("GunRun", true);
+					my_animator.SetBool ("GunIdle", false);
+				}
+			} else if (isGrounded) {				//Idle, if player is on ground
+				my_animator.SetBool ("GunIdle", true);
+				my_animator.SetBool ("GunRun", false);
+			}
+
 		}
-		//Testing Double Jump, needs some work
-		//else if(Input.GetButton("Jump") && (isGrounded == false)){
+		else {
+
+			if (Input.GetButton ("Jump") && isGrounded) {	//Jumping
+				my_animator.SetBool ("isJumpingB", true);
+				my_animator.SetBool ("isWalkingB", false);
+				my_animator.SetBool ("isIdleB", false);
+
+				//rb.AddForce (new Vector2 (0f, 500f));
+				rb.velocity = new Vector2 (0f, jumpheight);
+				isGrounded = false;
+			}
+			//Testing Double Jump, needs some work
+			//else if(Input.GetButton("Jump") && (isGrounded == false)){
 			//rb.velocity = new Vector2 (0f, jumpheight);
-		//}
+			//}
 
-		else if (moveHorizontal > 0){		//Moving right (while jumping or running)
-			transform.localScale = new Vector3 (x_pos, y_pos, z_pos);
+		else if (moveHorizontal > 0) {		//Moving right (while jumping or running)
+				transform.localScale = new Vector3 (x_pos, y_pos, z_pos);
 
-			if (isGrounded) {				//Moving right while running
-				my_animator.SetBool ("isWalkingB", true);
-				my_animator.SetBool ("isIdleB", false);
+				if (isGrounded) {				//Moving right while running
+					my_animator.SetBool ("isWalkingB", true);
+					my_animator.SetBool ("isIdleB", false);
+				}
+			} else if (moveHorizontal < 0) {		//Moving left (while jumping or running)
+				transform.localScale = new Vector3 (-x_pos, y_pos, z_pos);
+
+				if (isGrounded) {				//Moving left while running
+					my_animator.SetBool ("isWalkingB", true);
+					my_animator.SetBool ("isIdleB", false);
+				}
+			} else if (isGrounded) {				//Idle, if player is on ground
+				my_animator.SetBool ("isIdleB", true);
+				my_animator.SetBool ("isWalkingB", false);
 			}
 		}
-		else if (moveHorizontal < 0){		//Moving left (while jumping or running)
-			transform.localScale = new Vector3 (-x_pos, y_pos, z_pos);
 
-			if (isGrounded) {				//Moving left while running
-				my_animator.SetBool ("isWalkingB", true);
-				my_animator.SetBool ("isIdleB", false);
-			}
-		} 
-		else if (isGrounded){				//Idle, if player is on ground
-			my_animator.SetBool ("isIdleB", true);
-			my_animator.SetBool ("isWalkingB", false);
-		}
+		// Letting Player go to main menu when pressing escape key
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			SceneManager.LoadScene (0);
 		}
+
 	}
 
 	//Checking to see if player is touching the ground
@@ -79,7 +116,11 @@ public class PlayerScript2 : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other){
 		if (other.gameObject.CompareTag ("floor")) {
 			isGrounded = true;
-			my_animator.SetBool ("isJumpingB", false);
+			if (Gun) {
+				my_animator.SetBool ("GunJump", false);
+			} else {
+				my_animator.SetBool ("isJumpingB", false);
+			}
 		}
 		//Removes health if player comes in contact with enemy
 		else if (other.gameObject.CompareTag ("enemy")) {
@@ -87,6 +128,14 @@ public class PlayerScript2 : MonoBehaviour {
 			if (health == 0) {
 				//Game Over
 			}
+
+		} //Checking to see if player picked up Gun 
+		else if (other.gameObject.CompareTag ("Gun")) {
+			Gun = true;
+			other.gameObject.SetActive (false);
+			my_animator.SetBool ("isJumpingB", false);
+			my_animator.SetBool ("isWalkingB", false);
+			my_animator.SetBool ("isIdleB", true);
 		}
 	}
 
